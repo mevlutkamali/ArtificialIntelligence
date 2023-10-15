87813402918f50b22e15ff8c4817dbc36427d519
mevlutkamali <kamalimevlut424@gmail.com>
2023-10-15 13:31:02 +0300

    ArtificialIntelligence

---
+++ Artificial Intelligence/01_dataAdding/dataAddiing.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 15 13:16:45 2023
+
+@author: kamal
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+datas = pd.read_csv('veriler.csv')
+print(datas)
+
+print("Finish!..")
